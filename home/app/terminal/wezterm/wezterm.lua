bcbc3323768c8613936a770a2cd3bf4ebc642c4c
ecomex <skoch@sks-concept.de>
2024-10-09 08:10:34 +0200

    updated flake

--- home/app/terminal/wezterm/wezterm.lua
+++ home/app/terminal/wezterm/wezterm.lua
@@ -22,7 +22,7 @@ config.colors = {
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
